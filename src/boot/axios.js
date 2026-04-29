18a40dd5730b3df7539841c36bbedfa4a2771e46
Leonel <lmbagolin@gmail.com>
2026-04-29 20:48:05 -0300

    AXIOS

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
   timeout: 10000,
-  // withCredentials: true
+  withCredentials: true
 });
 
 export default boot(({ app }) => {
